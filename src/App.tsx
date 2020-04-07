099b8653a731d5765307b2d3604653f5a75ee80e
Dubois <nd@asii.fr>
2020-04-07 15:21:57 +0200

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { rem } from 'polished';
+import styled from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const AppComponents = styled.div`
+    margin: ${rem(15)} ${rem(25)} ${rem(0)} ${rem(25)};
+`;
+
+function App() {
+    return <AppComponents>mmmmmmmm</AppComponents>;
+}
+
+export default App;
